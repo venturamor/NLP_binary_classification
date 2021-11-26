5d29432ac6665bf2675e9dd34628a740c4efe1cc
Michael Toker <dontokeron@gmail.com>
2021-11-26 16:40:21 +0200

    send_model gets 80% accuracy (all false)

--- HW1/second_model.py
+++ HW1/second_model.py
@@ -10,7 +10,9 @@ class Second_model(torch.nn.Module):
         '''
         super(Second_model, self).__init__()
         self.linear = torch.nn.Linear(inputSize, outputSize)
+        torch.nn.init.xavier_uniform(self.linear.weight)
 
     def forward(self, x):
         out = self.linear(x)
+        out = torch.sigmoid(out)
         return out
